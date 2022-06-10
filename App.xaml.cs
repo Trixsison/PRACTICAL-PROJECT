48309ece2cd828d1198709cef1781020bb92f291
Trixsison <83437008+Trixsison@users.noreply.github.com>
2022-06-10 14:56:25 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_PRACTICALPROJECT
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new View.ShowAnimalPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
